cfb3e5c673ccf977a70829737a691769bea6fa27
ThunderDev1 <sgranger1@gmail.com>
2020-11-04 23:10:28 +0100

    Added minimalist opponents block (#2)

---
+++ app/src/components/Opponents.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Opponents">
+    <div v-for="player in players" :key="player.playerId">
+      {{ player.playerId }}
+      <div :class="['Player', { 'Player--Current': player.isCurrentPlayer }]">
+        {{ player.cardCount }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Opponents',
+  props: {
+    players: Object
+  }
+};
+</script>
+<style lang="scss">
+.Opponents {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  .Player {
+    border: 0.1em solid black;
+    height: 15em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &.Player--Current {
+      border: 0.5em solid black;
+    }
+  }
+}
+</style>
