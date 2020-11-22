4b992e40c7a7e00d4680fdd8840704f0c136c2a5
Adrien Boutigny <adrien.boutigny@gmail.com>
2020-11-22 16:15:34 +0100

    fix: weird router bug

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,29 +1,35 @@
 <template>
-  <h1>About</h1>
-  <p>
-    Bursting Felines is a clone of a game called "Exploging Kittens". The source
-    code is available on its
-    <a href="https://github.com/3-Devs-1-Bug/bursting-felines">
-      Github Repository</a
-    >.
-  </p>
+  <!-- 
+    For some reason, route transition is all fucked up if we dont wrap the 
+    whole view in a single element.
+  -->
+  <div>
+    <h1>About</h1>
+    <p>
+      Bursting Felines is a clone of a game called "Exploging Kittens". The
+      source code is available on its
+      <a href="https://github.com/3-Devs-1-Bug/bursting-felines">
+        Github Repository</a
+      >.
+    </p>
 
-  <h2>TODO</h2>
-  <ul class="todolist">
-    <li><del>Main game loop</del></li>
-    <li><del>Perish card</del></li>
-    <li><del>Resurect card</del></li>
-    <li><del>Skip card</del></li>
-    <li><del>Shuffle card</del></li>
-    <li>Deny card</li>
-    <li>Attack card</li>
-    <li>Loot card</li>
-    <li>Peek card</li>
-    <li>Combo cards</li>
-    <li>Handle the endgame</li>
-    <li>Private lobby system</li>
-    <li>Handle disconnections</li>
-  </ul>
+    <h2>TODO</h2>
+    <ul class="todolist">
+      <li><del>Main game loop</del></li>
+      <li><del>Perish card</del></li>
+      <li><del>Resurect card</del></li>
+      <li><del>Skip card</del></li>
+      <li><del>Shuffle card</del></li>
+      <li>Deny card</li>
+      <li>Attack card</li>
+      <li>Loot card</li>
+      <li>Peek card</li>
+      <li>Combo cards</li>
+      <li>Handle the endgame</li>
+      <li>Private lobby system</li>
+      <li>Handle disconnections</li>
+    </ul>
+  </div>
 </template>
 
 <script>
